1f4c426d248746dd16331783c15046a0278264c5
Yudi <madewahyudi22@gmail.com>
2020-06-21 14:05:00 +0800

    added header style desktop

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,5 @@
 window.addEventListener('load', () => {
-  const menu = document.getElementById('menu');
   const menuBtn = document.getElementById('menu-btn');
-  const menuLink = document.getElementById('menu-link');
-
-  menu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-  });
+  menuBtn.addEventListener('click', () => menuBtn.classList.toggle('active'));
 });
 
