d9f640a34b20b56d1de22afb0e0baaef9ceaf86d
Yudi <madewahyudi22@gmail.com>
2020-07-24 15:45:55 +0800

    Updates

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,10 @@
-window.addEventListener('load', () => {
-  const menuBtn = document.getElementById('menu-btn');
-  const menu = document.getElementById('menu');
-  const menuLink = document.getElementById('menu-link');
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const menuLink = document.getElementById('menu-link');
 
-  function toggleActive() {
-    menu.classList.toggle('active');
-  }
-  
-  menuBtn.addEventListener('click', toggleActive);
-  menuLink.addEventListener('click', () => setTimeout(toggleActive, 100));
-});
+function toggleActive() {
+  menu.classList.toggle('active');
+}
 
+menuBtn.addEventListener('click', toggleActive);
+menuLink.addEventListener('click', () => setTimeout(toggleActive, 100));
